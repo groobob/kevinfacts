f05783dfc4cbf05f34879e218b9e54624bde3acf
groobob <elitegem84@gmail.com>
2023-07-12 19:50:25 -0400

    Fixed Netlify images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src="/Images/kevin.png" className="nav--icon" />
+            <img src="/images/kevin.png" className="nav--icon" />
             <h3 className="nav--logo_text">KevinFacts</h3>
             <h4 className="nav--title">The Dapperest Man Alive</h4>
         </nav>
