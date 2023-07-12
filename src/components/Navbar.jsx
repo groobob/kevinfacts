3f65a80599f69a06bb9831879f458ebf48f91394
groobob <elitegem84@gmail.com>
2023-07-12 19:34:39 -0400

    Trying to fix images for Netlify deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src="../public/kevin.png" className="nav--icon" />
+            <img src="/Images/kevin.png" className="nav--icon" />
             <h3 className="nav--logo_text">KevinFacts</h3>
             <h4 className="nav--title">The Dapperest Man Alive</h4>
         </nav>
